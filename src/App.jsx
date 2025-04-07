53dbd38c737cae04cfdf6a9b082fb1fe784dcd95
saritadipa34 <saritadipa34@gmail.com>
2025-04-07 19:53:17 +0530

    Calculation logic implemented partially

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 
+import { useState } from "react";
 import NumberBox from "./components/NumberBox";
 import ResultBox from "./components/ResultBox";
 
 const App=()=>{
+const [boxValue,setBoxValue]=useState('');
 
-const handleResult=()=>{
-  
+const handleResult=(value)=>{
+
 }
 
 const handleBox=(value)=>{
+  console.log(value,"clicked")
+  switch (value){
+    case "AC":
+      setBoxValue('');
+      break;
+      case "DE":
+        setBoxValue(boxValue.slice(0,-1))
+break;
+case "=":
+  setBoxValue(eval(boxValue));
+  break;
+  default :
+  setBoxValue(boxValue+value);
+  // setBoxValue(value);
+
+  }
   console.log(value,"clicked")
 }
 
   const signs=["AC","DE",".","/","7","8","9","*","4","5","6","+","1","2","3","-","00","0","="];
   return(
     <div className="h-[520px] w-[350px] py-[20px] px-[20px] bg-gray-400">
-      <ResultBox onChange={handleResult}/>
+      <ResultBox value={boxValue} onChange={handleResult}/>
       <div  className="flex flex-wrap gap-[10px]">
       {
         signs.map((sign,index)=>{
--- src/components/ResultBox.jsx
+++ src/components/ResultBox.jsx
@@ -1,7 +1,7 @@
 
 const ResultBox=({value,onChange})=>{
     return(
-        <input type="text" onChange={onChange} className="bg-white w-full h-[70px] px-[15px] py-[10px] text-5xl mb-[20px] "
+        <input type="text" onChange={onChange} className="bg-white text-right w-full h-[70px] px-[15px] py-[10px] text-5xl mb-[20px] "
 value={value}
         />
     )
